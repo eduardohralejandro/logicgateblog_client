f5e3ab5ebfebbe080bc4ed2ac85992e45151e376
Eduardo Hernandez <eduardo@chatbot-plus.com>
2024-03-08 08:44:29 +0100

    add text editor to the modal add article

--- admin/src/src/components/add_article_layout/AddArticleLayout.tsx
+++ admin/src/src/components/add_article_layout/AddArticleLayout.tsx
@@ -1,10 +1,16 @@
 import ButtonElement from "../button/ButtonElement";
 import { PlusCircleFilled } from "@ant-design/icons";
+import { Typography } from "antd";
 import { ListElement } from "../components";
+
+import useModalState from "../../hooks/useModalState";
+import { ModalElement, TextEditor, InputElement } from "../components";
 
 import styles from "./add_article_layout.module.scss";
 
 const AddArticleLayout = () => {
+  const { open, showModal, handleOk, handleCancel } = useModalState();
+
   const customStyles = {
     backgroundColor: "#52d64b",
     color: "white",
@@ -22,6 +28,24 @@ const AddArticleLayout = () => {
             style={customStyles}
             text="Add article"
             icon={<PlusCircleFilled />}
+            onClick={showModal}
+          />
+          <ModalElement
+            title="Add article"
+            open={open}
+            handleCancel={handleCancel}
+            handleOk={handleOk}
+            children={
+              <>
+                <Typography.Title level={5}>Title</Typography.Title>
+                <InputElement />
+                <Typography.Title style={{ marginTop: "1rem" }} level={5}>
+                  Content
+                </Typography.Title>
+                <TextEditor />
+              </>
+            }
+            footer={<footer>Footer goes here</footer>}
           />
         </div>
         <ListElement />
