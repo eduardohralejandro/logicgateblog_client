0021f55fb57553e0581d249fbd966eb10225508b
Eduardo Hernandez <eduardo@chatbot-plus.com>
2024-03-27 15:52:39 +0100

    add new productionurl from vercel json

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -3,19 +3,27 @@ import react from "@vitejs/plugin-react";
 
 const apiUrl = process.env.VITE_VERCEL_URL || "http://localhost:5173";
 
+function getBaseUrl() {
+  return process.env.VERCEL_ENV === "production"
+    ? process.env.VITE_VERCEL_URL
+    : process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : ` http://localhost:5173`;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: apiUrl,
+        target: getBaseUrl(),
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
     cors: {
-      origin: apiUrl,
+      origin: getBaseUrl(),
       methods: ["GET", "POST"],
       allowedHeaders: ["Content-Type", "Authorization"],
       preflightContinue: true,
