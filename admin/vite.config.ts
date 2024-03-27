df48048151e9315cb6898a030f91b7e5996e4461
Eduardo Hernandez <eduardo@chatbot-plus.com>
2024-03-27 16:57:42 +0100

    add new productionurl from vercel json

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -20,14 +20,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: apiUrl,
+      "/testapi": {
+        target: "https://logicgatesblog-9da8a03a3c58.herokuapp.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
     cors: {
-      origin: apiUrl,
+      origin: "https://logicgatesblog-9da8a03a3c58.herokuapp.com",
       methods: ["GET", "POST"],
       allowedHeaders: ["Content-Type", "Authorization"],
       preflightContinue: true,
