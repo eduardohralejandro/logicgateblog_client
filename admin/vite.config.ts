a1494535f8542a266251b85079c36473788d55f2
Eduardo Hernandez <eduardo@chatbot-plus.com>
2024-03-27 17:08:18 +0100

    add new productionurl from vercel json

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -7,20 +7,13 @@ console.log("on vite vercel url " + process.env.VITE_VERCEL_URL);
 if (process.env.VITE_VERCEL_URL) {
   console.log("on vite vercel url " + process.env.VITE_VERCEL_URL);
 }
-// function getBaseUrl() {
-//   return process.env.VERCEL_ENV === "production"
-//     ? "https://logicgatesblog-9da8a03a3c58.herokuapp.com"
-//     : process.env.VERCEL_URL
-//     ? `https://${process.env.VERCEL_URL}`
-//     : ` http://localhost:5173`;
-// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/testapi": {
+      "/api": {
         target: "https://logicgatesblog-9da8a03a3c58.herokuapp.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
