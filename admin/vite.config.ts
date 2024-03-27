e64eeb2bc030fdda03ea72a0413c5c3bfa0c8269
Eduardo Hernandez <eduardo@chatbot-plus.com>
2024-03-27 16:24:14 +0100

    add new productionurl from vercel json

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 const apiUrl = process.env.VITE_VERCEL_URL || "http://localhost:5173";
 
-function getBaseUrl() {
-  return process.env.VERCEL_ENV === "production"
-    ? "https://logicgatesblog-9da8a03a3c58.herokuapp.com"
-    : process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : ` http://localhost:5173`;
-}
+// function getBaseUrl() {
+//   return process.env.VERCEL_ENV === "production"
+//     ? "https://logicgatesblog-9da8a03a3c58.herokuapp.com"
+//     : process.env.VERCEL_URL
+//     ? `https://${process.env.VERCEL_URL}`
+//     : ` http://localhost:5173`;
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,13 +17,13 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://logicgatesblog-9da8a03a3c58.herokuapp.com",
+        target: apiUrl,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
     cors: {
-      origin: "https://logicgatesblog-9da8a03a3c58.herokuapp.com",
+      origin: apiUrl,
       methods: ["GET", "POST"],
       allowedHeaders: ["Content-Type", "Authorization"],
       preflightContinue: true,
